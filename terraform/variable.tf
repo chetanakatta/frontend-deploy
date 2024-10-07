0bae87bfb788dfc00c70bd4bd3e530247da4fcb9
chetana <chetana@gmail.com>
2024-10-07 15:03:49 -0400

    testing

---
+++ terraform/variable.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+    default = "expense.fun"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+        Component = "frontend"
+    }
+}
+
+variable "app_version" {
+    
+}
