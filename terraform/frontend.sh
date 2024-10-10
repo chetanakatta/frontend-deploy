e1a89cca1f33001494b8861a9fc15e3a1b0c0a65
chetana <chetana@gmail.com>
2024-10-10 14:57:52 -0400

    testing

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -4,4 +4,4 @@ environment=$2
 appVersion=$3
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/chetanakatta/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$3
+ansible-pull -i localhost, -U https://github.com/chetanakatta/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$appVersion
